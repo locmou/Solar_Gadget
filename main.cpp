8a28ad8ca7bcdeb23040017ccb5d29b1148ca662
locmou <loic.mounier@laposte.net>
2024-11-01 19:54:24 +0100

    premire rsdaa

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
 #include <Arduino.h>
 
+#define NUM_OF_DIGITS 4
 
-int incomingByte = 0; // variable pour lecture de l'octet entrant
+int latch = 4; //74HC595  pin 9 STCP
+int cs = 5; //74HC595  pin 10 SHCP
+int data = 3; //74HC595  pin 8 DS
+int dPins[4] = {11, 10, 9, 8};
+
+//  DP G F E D C B A
+//0: 1 1 0 0 0 0 0 0 0xc0
+//1: 1 1 1 1 1 0 0 1 0xf9
+//2: 1 0 1 0 0 1 0 0 0xa4
+//3: 1 0 1 1 0 0 0 0 0xb0
+//4: 1 0 0 1 1 0 0 1 0x99
+//5: 1 0 0 1 0 0 1 0 0x92
+//6: 1 0 0 0 0 0 1 0 0x82
+//7: 1 1 1 1 1 0 0 0 0xf8
+//8: 1 0 0 0 0 0 0 0 0x80
+//9: 1 0 0 1 0 0 0 0 0x90
+unsigned char table[] =
+{0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 
 void setup() {
-  Serial.begin(9600); // ouvre le port série et fixe le débit à 9600 bauds
+  Serial.begin(115200);
+  pinMode(latch, OUTPUT);
+  pinMode(cs, OUTPUT);
+  pinMode(data, OUTPUT);
+  for (int j = 0; j < NUM_OF_DIGITS; j++) pinMode(dPins[j], OUTPUT);
 }
 
 void loop() {
-  // envoie une donnée sur le port série seulement quand reçoit une donnée
-  if (Serial.available() > 0) { // si données disponibles sur le port série
-    // lit l'octet entrant
-    incomingByte = Serial.read();
-
-    // renvoie l'octet reçu
-    Serial.print("Octet recu: ");
-    Serial.println((char)incomingByte, DEC);
+  //Count from 0 to 9 on each digit
+  for (int i = 0; i < NUM_OF_DIGITS; i++) {
+    for (int j = 0; j < 10; j++) {
+      Display(i, j);
+      delay(500);
+      Serial.println(j);
+    }
+    delay(500);
   }
 }
+
+void Display(int id, unsigned char num)
+{
+  digitalWrite(latch, LOW);
+  shiftOut(data, cs, MSBFIRST, table[num]);
+  digitalWrite(latch, HIGH);
+  for (int j = 0; j < NUM_OF_DIGITS; j++) digitalWrite(dPins[j], LOW);
+  digitalWrite(dPins[id], HIGH);
+}
+
